e54f59e997261fe3aa0921fe541e41f86c51895e
Adrian Walker <adrianwalker@MacBook-Pro.local>
2022-04-08 10:43:13 -0400

    created components

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
 
+
 function App() {
   return (
     <Router>
