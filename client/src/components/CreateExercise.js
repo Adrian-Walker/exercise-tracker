b8a06fdc880ba526bcc05cc334c32283ab04ba1d
Adrian Walker <adrianwalker@MacBook-Pro.local>
2022-04-08 21:30:50 -0400

    updated create exercise log component

--- client/src/components/CreateExercise.js
+++ client/src/components/CreateExercise.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 export default class CreateExercise extends Component {
     constructor(props) {
@@ -74,7 +76,39 @@ export default class CreateExercise extends Component {
                             }
                         </select>
                     </div>
-
+                    <div className="form-group">
+                        <label>Description</label>
+                        <input type='text'
+                            required
+                            className="form-control"
+                            value={this.state.description}
+                            onChange={this.onChangeDescription}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Duration (in minutes): </label>
+                        <input
+                            type='text'
+                            className="form-control"
+                            value={this.on}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Date: </label>
+                        <div>
+                            <DatePicker
+                                selected={this.state.date}
+                                onChange={this.onChangeDate}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                type='submit'
+                                value='Create Exercise Log'
+                                className="btn btn-primary"
+                            />
+                        </div>
+                    </div>
                 </form>
             </div>
         )
