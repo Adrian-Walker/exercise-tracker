c096ae2c69ef5a400a962b068d29053afc43088a
Adrian Walker <adrianwalker@MacBook-Pro.local>
2022-04-09 03:10:08 -0400

    updated create user

--- client/src/components/CreateUser.js
+++ client/src/components/CreateUser.js
@@ -1,10 +1,39 @@
 import React, { Component } from "react";
 
 export default class CreateUser extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: '',
+        }
+    }
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const user = {
+            username: this.setState.username
+        }
+        console.log(user);
+    }
+
+
+
+
     render() {
         return (
             <div>
-                <h1>Create User Component</h1>
+
             </div>
         )
     }
